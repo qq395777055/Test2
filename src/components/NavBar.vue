165f8a5466825929e155a0533a27aaae28ba9850
陈皓 <395777055@qq.com>
2017-05-01 22:36:55 +0800

    增加了右侧画出用户菜单的二级菜单列表，并且解决了时间冒泡的问题，但是有点治标不治本。。。。

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -5,8 +5,8 @@
 			<li class="logo">LOGO</li>
 			<li class="search">搜索</li>
 		</ul>
-		<div id="box" @click="ch2">
-			<div class="left-box">
+		<div id="box"  @click="ch2()">
+			<div class="left-box" >
 				<div id="user">
 					<div class="Head-portrait">头像</div>
 					<ul class="user-list">
@@ -17,7 +17,17 @@
 				</div>
 				<ul class="list">
 					<li>首页</li>
-					<li>魔法定制</li>
+					<li @click.stop="mofaListopen()">魔法定制
+						<span>></span>
+						<ul id="mofaList">
+							<li>衬衣定制</li>
+							<li>套西定制</li>
+							<li>单西定制</li>
+							<li>马甲定制</li>
+							<li>西裤定制</li>
+							<li>大衣定制</li>
+						</ul>
+					</li>
 					<li>兑换专区</li>
 					<li>关于虾想</li>
 				</ul>
@@ -39,15 +49,15 @@
 				$("#box").css("display","block")
 				$(".left-box").animate({left:0})
 			},
-			ch2(){	
-				
+			ch2(){
 				$(".left-box").animate({left:"-52%"},function () {
 					$("#box").css("display","none")
-				})
-				
-				
+					$("#mofaList").css("display","none")
+				})			
+			},
+			mofaListopen(){
+				$("#mofaList").toggle()
 			}
-			
 		}
 	}
 </script>
